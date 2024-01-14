b3a29226644ee74e546ec97975a3a59440707efa
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 17:51:01 +0200

    Renamed function

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,9 +1,9 @@
 // Get one random element from an array
 'use strict'
 
-const SAMPLE = (arr) => {
+const sample = (arr) => {
 
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-module.exports = SAMPLE;
+module.exports = sample;
