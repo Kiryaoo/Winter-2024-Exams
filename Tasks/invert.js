d1f6c1fe0b9eca87426cf5639ef41b186d7a8956
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 16:31:56 +0200

    Add 'use strict' and const type

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,7 +1,9 @@
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
+'use strict'
+
+const invert = (A, i, j, k) => {
+ const T = Object.keys(A, 4);
   T.forEach((_, i) => {
     T[i] = A.pop();
     ((x) => {
