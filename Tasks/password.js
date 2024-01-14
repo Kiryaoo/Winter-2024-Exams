5239f18e6225c77c8b16eaaa9ae58f530ad7e81d
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 14:20:58 +0200

    Optimized code for make it more readable

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,12 +3,12 @@
 'use strict'
 
 const generatePassword = (alphabet, length) => {
-
   let key = '';
+
   for (let i = 0; i < length; i++)
   {
     const index = Math.floor(Math.random() * alphabet.length);
-    key = key + alphabet[index];
+    key += alphabet[index];
   }
   return key;
 }
