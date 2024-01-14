2d990b9f78ebca93b694a7bb62a0107a0f09828a
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 18:00:54 +0200

    Replace loop 'for' by 'while' to simplified and optimization

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,18 +1,17 @@
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function (a = [], b = []) {
+'use strict'
+const zip = (arr1 = [], arr2 = []) => {
+  const result = [];
+
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
-    if (i < j) {
-      delete a[i++];
-    } else {
-      (() => (b[j++] = CELL))();
-    }
-    if (CELL[0] == undefined) b.length -= 1
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    result.push([arr1[i++], arr2[j++]]);
   }
-  return b;
+
+  return result;
 };
 
 module.exports = zip;
