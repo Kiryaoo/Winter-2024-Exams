a91478d40bb750108bba3178a0093ec49ad6a445
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 14:32:21 +0200

    Apply proper code style

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,18 +1,14 @@
 // Extract substring between prefix and suffix
-'use strict'
+"use strict";
 
 const getValueBetween = (str, prefix, suffix) => {
-
   if (str.indexOf(prefix) === -1) {
-    return '';
-  }
-  else {
-
-    str = str.substring(str.indexOf(prefix)+prefix.length);
+    return "";
+  } else {
+    str = str.substring(str.indexOf(prefix) + prefix.length);
     if (suffix) {
-
       if (str.indexOf(suffix) === -1) {
-        return '';
+        return "";
       } else {
         str = str.substring(0, str.indexOf(suffix));
       }
