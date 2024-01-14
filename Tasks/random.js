db4c6a2f4b5b76d2341ae466ca2a62dc406e3d97
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 11:55:55 +0200

    Optimization code by removing conditions

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,13 +2,8 @@
 'use strict';
 
 const random = (min, max) => {
-  let result;
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-}
 
+  let result = min + Math.floor(Math.random() * (max - min + 1));
+  return result;
+}
 module.exports = random;
