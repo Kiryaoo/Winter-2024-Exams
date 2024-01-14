2ad0bccf9d775d8096d9a17aeaa46954b590fcef
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 12:24:54 +0200

    Removed useless variable and change it to method in loop

--- Tasks/month.js
+++ Tasks/month.js
@@ -4,8 +4,8 @@
 const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
 const Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++)
+
+  for (let i = 0; i < Months.length; i++)
   {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
