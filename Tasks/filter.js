fca97a7484f54b70884d3733e7e78d3c13880810
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-14 16:29:33 +0200

    Apply proper code style

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,23 +1,21 @@
 // Filter array by type name
 
-'use strict'
+"use strict";
 
 // Filter array by type name
 const filterArrayByTypeName = (typeArray, typeName) => {
   let removeElem = [];
 
-
   for (const elem of typeArray) {
     if (typeof elem != typeName) {
-      const index  = typeArray.indexOf(elem);
+      const index = typeArray.indexOf(elem);
       removeElem.unshift(index);
     }
   }
 
-  for (const elem of removeElem){
+  for (const elem of removeElem) {
     typeArray.splice(elem, 1);
   }
-
 
   return typeArray;
 };
