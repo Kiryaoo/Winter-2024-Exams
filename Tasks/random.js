9a3b0f56199a155cb40ff3e972d6a2193378cfd3
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-16 19:15:04 +0200

    Adjusted function

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const random = (min, max) => {
-  const result = min + Math.floor(Math.random() * (max - min + 1));
-  return result;
+
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 module.exports = random;
