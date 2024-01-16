5fe4cea8486378f1db4195e959e4f836433338f3
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2024-01-16 15:43:15 +0200

    Apply correct code style

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,13 +1,23 @@
 // Get month number
-'use strict'
-
-
+"use strict";
 
 const monthOrder = (stringOfMonth) => {
-  const months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
+  const months = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ];
 
-  for (let i = 0; i < months.length; i++)
-  {
+  for (let i = 0; i < months.length; i++) {
     if (stringOfMonth.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
